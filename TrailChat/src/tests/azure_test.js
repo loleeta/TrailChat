2ae776ed28e5f2e60343922d7d7d5fb56c6095ed
loleeta <lolitalam3@gmail.com>
2018-06-11 15:53:57 -0700

    updating azure test cases

--- TrailChat/src/tests/azure_test.js
+++ TrailChat/src/tests/azure_test.js
@@ -10,53 +10,56 @@ var http = require('http');
 chai.use(chaiHttp);
 
 describe('TrailChat Azure tests', function () {
-	this.timeout(15000);
+  this.timeout(15000);
 
-	var requestResult;
-	var response;
+  var requestResult;
+  var response;
 
-    before(function (done) {
+  before(function (done) {
+    chai.request('http://trailchatclt.azurewebsites.net')
+      .get("/messages")
+      .end(function (err, res) {
+        requestResult = res.body;
+        response = res;
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        response.body.should.be.a('array');
+        response.should.be.json;
+        done();
+      });
+  });
 
-//		chai.request('http://trailchatclt.azurewebsites.net')
-        chai.request("http://localhost")
-			.get("/app/lists")
-			.end(function (err, res) {
-				requestResult = res.body;
-				response = res;
-            //expect(err).to.be.null;
-            //expect(res).to.have.status(200);
-                console.log(res.request.url);
-				done();
-			});
-	});
+  it('Should return an array of messages', function () {
+    expect(response).to.exist;
+    expect(response).to.have.status(200);
+    expect(response).to.be.an('object');
+    expect(response.body).to.have.length.above(5);
+    expect(response).to.have.headers;
+  });
 
-    it('Should return an array object with three objects', function (){
-//		expect(response).to.have.status(200);
-        expect(response.body).to.be.json; //an.object;
-		expect(response.body).to.have.length.above(1);
-//		expect(response).to.have.headers;
-    });
-    /*
-	it('The first entry in the array has known properties', function(){
-		expect(requestResult[0]).to.include.keys('name');
-		expect(response).to.have.deep.property('body[0].listId', '1');
-		expect(response.body).to.not.be.a.string;
-		//done();
-	});
-*/
-	it('The elements in the array have the expecte properties', function(){
-		expect(response.body).to.satisfy(
-			function (body) {
-				for (var i = 0; i < body.length; i++) {
-					expect(body[i]).to.have.property('name');
-					expect(body[i]).to.have.property('description');
-					expect(body[i]).to.have.property('listId');
-					expect(body[i]).to.have.property('state').to.have.length(1);
-					expect(body[i]).to.have.property('owner').that.is.a('string');
-				}
-				return true;
-			});
-		//done();
-	});
+  it('The first entry in the messages array has known properties', function () {
+    expect(requestResult[0]).to.include.keys('_id');
+    expect(requestResult[0]).to.include.property('message_content');
+    expect(requestResult[0]).to.include.property('message_type');
+    expect(requestResult[0]).to.include.property('message_date');
+    expect(requestResult[0]).to.include.property('message_id');
+    expect(requestResult[0]).to.include.property('chat_id');
+    expect(response.body).to.not.be.a.string;
+  });
+
+  it('The elements in the messages array have the expected properties', function () {
+    expect(response.body).to.satisfy(
+      function (body) {
+        for (var i = 0; i < body.length; i++) {
+          expect(body[i]).to.have.property('message_content');
+          expect(body[i]).to.have.property('message_type');
+          expect(body[i]).to.have.property('message_date');
+          expect(body[i]).to.have.property('message_id');
+          expect(body[i]).to.have.property('message_id').that.is.a('number');
+        }
+        return true;
+      });
+    //done();
+  });
 
 });
